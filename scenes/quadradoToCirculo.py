8820bee2eca0252ad3aebfc2f817b9ecca4bda9e
Victor Oliveira <victoroliver249@gmail.com>
2026-01-21 23:55:59 -0300

    Remove Quadrado class and its animation

--- scenes/quadradoToCirculo.py
+++ scenes/quadradoToCirculo.py
@@ -1,18 +1,1 @@
-from manim import *
-
-class Quadrado(Scene):
-    def construct(self):
-
-        quadrado = Square()
-        quadrado.set_fill(PURPLE, opacity=0.5)
-        quadrado.rotate(PI/4)
-
-        circulo = Circle()
-        circulo.set_fill(BLUE, opacity=0.5)
-
-        self.play(Create(quadrado))
-        self.play(Transform(quadrado, circulo))
-
-        self.wait(1)
-
 
